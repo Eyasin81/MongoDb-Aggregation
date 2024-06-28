496458a673475667f749748c70ba29f8ec4327d5
Eyasin81 <eyasina81@gmail.com>
2024-06-28 16:21:30 +0600

    Single or Multi data Insert

---
+++ insertData.js
@@ -0,0 +1,17 @@
+//Single Data insert 
+db.products.insertOne({
+   "Name" : "Eyasin ALi",
+   "Roll" : "3033",
+   "Reg" : "REG019",
+   "City" : "Naogaon"
+})
+// multi Data Insert Collection
+db.products.insertMany({
+   "Name" : "Eyasin ALi",
+   "Roll" : "3033",
+   "Reg" : "REG019",
+   "City" : "Naogaon"
+})
+   
+   
+   
